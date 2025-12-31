8f66159696e540039a0d44f6ef5d45d492a7700b
oussama <khelefoussama2005@gmail.com>
2025-12-31 11:38:11 +0100

    edit oneclickdz class

--- src/classes/oneClickdz.class.ts
+++ src/classes/oneClickdz.class.ts
@@ -1,5 +1,7 @@
 import { Axios } from "axios";
 import axios from "axios";
+import { sleep } from "bun";
+import { v4 as uuidV4 } from "uuid";
 
 class OneClickDz {
     private apiKey: string;
@@ -13,16 +15,88 @@ class OneClickDz {
             baseURL: this.baseUrl,
             headers: {
                 'Content-Type': 'application/json',
-                "X-Access-Token" : this.apiKey,
+                "X-Access-Token": this.apiKey,
             },
         });
     }
 
-    // load availble plans
-    loadPlans(){
-        this.httpClient.get('mobile/planes')
-        .then((response) => console.log(response.data))
-        .catch((error) => console.log("Error loading plans"));
+    // send top up request
+    async sendTopUp(planCode: string, phoneNumber: string, amount: number) {
+        const ref = `${new Date().toUTCString}-${uuidV4()}`;
+        this.httpClient.post('mobile/send', {
+            "plan_code": planCode,
+            "MSSIDN": phoneNumber,
+            "amount": amount,
+            "ref": ref,
+        }).then((response) => {
+            const data = response.data;
+            return data.data;
+        }).catch((error) => {
+            return;
+        });
+        /* const success = {
+             "success": true,
+             "data": {
+                 "topupId": "69537bd879b32384c0bfb932",
+                 "topupRef": "API-+213658535537-ref-124"
+             },
+             "meta": {
+                 "timestamp": "2025-12-30T07:14:32.960Z"
+             }
+         }
+         const faile = {
+             "success": false,
+             "error": {
+                 "code": "DUPLICATED-REF",
+                 "message": "This reference ID is already in use."
+             },
+             "requestId": "req-fpg"
+         }*/
+    }
+    async checkStatusById(id: string) {
+        try {
+            const response = await this.httpClient.get(`mobile/check-id/${id}`);
+            return response.data;
+        } catch (err) {
+            return;
+        }
+    }
+
+    async polling(id: string, maxAttempts = 60) {
+        for (let i = 0; i < maxAttempts; i++) {
+            const data = await this.checkStatusById(id);
+            if (!data) {
+                await sleep(5000);
+                continue;
+            }
+            if (data.status === "FULFILLED") {
+                return {
+                    success: true,
+                    msg: "Top up successful",
+                };
+            } else if (data.status === 'REFUNDED') {
+                return {
+                    success: false,
+                    msg: data.refund_message as string,
+                };
+            } else if (data.status === "UNKNOWN_ERROR") {
+                return {
+                    success : false,
+                    msg : "Something went wrong , contact the support",
+                }
+            }else {
+                await sleep(5000);
+            }
+        }
+    }
+
+    async checkBalance(){
+        try {
+            const response = await this.httpClient.get(`account/balance`);
+            return response.data.balance;
+        } catch (err) {
+            return;
+        }
     }
 }
 
